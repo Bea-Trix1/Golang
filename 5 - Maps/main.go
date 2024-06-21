def55d3ce8ed7c66edcae3bc09beef96413b9cde
Beatriz Ferrante <beatriz.ferrante37@gmail.com>
2024-06-21 20:15:46 -0300

    maps

--- 5 - Maps/main.go
+++ 5 - Maps/main.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func main() {
 
-	salarios := map[string]int{"Beatriz": 1000, "Joao": 100}
+	salarios := map[string]int{"Beatriz": 1000, "Bea": 100}
 
 	fmt.Println("salarios: ", salarios["Beatriz"])
 
 	sal := make(map[string]int)
 
-	sal["Joao"] = 200
+	sal["Bea"] = 200
 
 	for _, s := range sal {
 		fmt.Println(s)
