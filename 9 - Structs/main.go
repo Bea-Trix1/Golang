59d2dee00b3237c57b247527d0d5d7c893f46d5a
Beatriz Ferrante <beatriz.ferrante37@gmail.com>
2024-06-22 14:21:42 -0300

    Structs

--- 9 - Structs/main.go
+++ 9 - Structs/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Estrutura{1, "bia"})
+	v := Estrutura{1, "Algo"}
+	v.Valor = 10
+	fmt.Println(v.Valor)
+	v.Produto = "Sabonete" // Campos Struct são acessados ​​através de um ponto.
+	fmt.Println(v.Produto)
 }
 
 // A struct é uma coleção de campos.
