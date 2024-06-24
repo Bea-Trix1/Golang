66e2da82ba37317f82264239723aabcbee9f6810
Beatriz Ferrante <beatriz.ferrante37@gmail.com>
2024-06-24 20:28:34 -0300

    Exercicio contador de palavras Maps

---
+++ 16.2 - Exercicio Maps/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Exercício: Maps
+// Implementar WordCount. Ele deve retornar um map das contagens de cada "word" na string s. A função wc.Test executa um conjunto de testes contra a função fornecida e imprime o sucesso ou falha.
+// Você pode achar strings.Fields útil.
+
+func WordCount(s string) map[string]int {
+	// Dividindo a string em palavras
+	palavras := strings.Fields(s) // strings.Fields(s): Esta função divide a string s em palavras, retornando um slice de strings.
+
+	// Criando um map para armazenar as contagens
+	contador := make(map[string]int)
+
+	// Iterando sobre as palavras e atualizando as contagens
+	for _, palavra := range palavras { //Itera sobre cada palavra no slice palavra.
+		contador[palavra]++ // Incrementa a contagem da palavra no map.
+	}
+	//Retornando o map
+	return contador
+}
+
+func main() {
+	s := "Eu amo a Laura barros!"
+	fmt.Println(WordCount(s))
+}
+
+// Explicação
+
+// make(map[string]int): Cria um mapa para armazenar as contagens de palavras.
+// for _, word := range words: Itera sobre cada palavra no slice words.
+// wordCount[word]++: Incrementa a contagem da palavra no mapa.
