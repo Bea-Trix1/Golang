5f7f88fdec1c7cb680bcbc3c997974c8d4040ff2
Beatriz Ferrante <beatriz.ferrante37@gmail.com>
2024-09-23 19:13:00 -0300

    Generics

---
+++ 21 - Generics/main.go
@@ -0,0 +1,22 @@
+package main
+
+// Tipos Generics são formas mais práticas de vc não ter que criar duas funções para tipos diferentes
+// Utilizando o generics, vc pode criar uma função que aceita qualquer tipo de dado, sem precisar criar uma função para cada tipo.
+func Soma[T int | float64](m map[string]T) T {
+	var sum T
+
+	for _, v := range m {
+		sum += v
+	}
+	return sum
+}
+
+// Exemplo de uso:
+// A função Soma aceita tanto um map de int quanto um map de float64
+func main() {
+	m := map[string]int{"a": 1, "b": 2, "c": 3}
+	println(Soma(m))
+
+	m2 := map[string]float64{"a": 1.1, "b": 2.2, "c": 3.3}
+	println(Soma(m2))
+}
