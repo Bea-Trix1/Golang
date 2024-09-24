4d2aade0823b2d92e9230d91e558abdec586723d
Beatriz Ferrante <beatriz.ferrante37@gmail.com>
2024-09-23 21:31:08 -0300

    For

--- 22 - For/main.go
+++ 22 - For/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Percorrendo um slice com for e range
 func main() {
 	numeros := []string{"um", "dois", "três"}
@@ -7,6 +9,17 @@ func main() {
 	// Range vai percorrer o slice e retornar o índice e o valor
 	for c, v := range numeros {
 		print(c, " - ", v, "\n")
+	}
+
+	// While em Go - Nem existe while em Go, mas podemos simular com for
+	for i := 0; i < 10; i++ {
+		fmt.Print(i, "\n")
+	}
+
+	// Loop infinito - usado para por exemplo colocar um servidor para rodar, consumir uma fila, etc
+	for {
+		fmt.Print("Loop infinito\n")
+		break // Para o loop
 	}
 
 }
